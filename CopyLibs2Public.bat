9b04012e1b7da9679dc7d5e25bbb4666665abbd9
Jérôme Gorin <gorinje@gmail.com>
2015-07-26 12:42:13 +0200

    Update CopyLibs2public

--- CopyLibs2Public.bat
+++ CopyLibs2Public.bat
@@ -53,29 +53,29 @@ echo Copying x86 extralibs
 copy lib\win32\release\*eay* lib\win32\debug\
 copy lib\win32\release\av* lib\win32\debug\
 copy lib\win32\release\swscale* lib\win32\debug\
-copy lib\win32\debug\*.lib ..\gpac_public\extra_lib\lib\win32\debug\
-copy lib\win32\release\*.lib ..\gpac_public\extra_lib\lib\win32\release\
-copy lib\win32\debug\*.dll ..\gpac_public\bin\win32\debug\
-copy lib\win32\release\*.dll ..\gpac_public\bin\win32\release\
-copy lib\win32\release\*.manifest ..\gpac_public\bin\win32\release\
-copy lib\win32\debug\*.plg ..\gpac_public\bin\win32\debug\
-copy lib\win32\release\*.plg ..\gpac_public\bin\win32\release\
+copy lib\win32\debug\*.lib ..\Access_open\extra_lib\lib\win32\debug\
+copy lib\win32\release\*.lib ..\Access_open\extra_lib\lib\win32\release\
+copy lib\win32\debug\*.dll ..\Access_open\bin\win32\debug\
+copy lib\win32\release\*.dll ..\Access_open\bin\win32\release\
+copy lib\win32\release\*.manifest ..\Access_open\bin\win32\release\
+copy lib\win32\debug\*.plg ..\Access_open\bin\win32\debug\
+copy lib\win32\release\*.plg ..\Access_open\bin\win32\release\
 if exist xulrunner-7.0.1.en-US.win32.sdk.zip goto XULSDK_copy
 wget --no-check-certificate https://ftp.mozilla.org/pub/mozilla.org/mozilla.org/xulrunner/releases/7.0.1/sdk/xulrunner-7.0.1.en-US.win32.sdk.zip
 unzip -n xulrunner-7.0.1.en-US.win32.sdk.zip
 :XULSDK_copy
-xcopy /i /e /q /y xulrunner-sdk ..\gpac_public\extra_lib\include\xulrunner-sdk\
+xcopy /i /e /q /y xulrunner-sdk ..\Access_open\extra_lib\include\xulrunner-sdk\
 :DekTec_copy
 unzip -n dektec_dtapi_5.2.zip
 if exist SDL-devel-1.2.15-VC.zip goto SDL_copy
 wget http://www.libsdl.org/release/SDL-devel-1.2.15-VC.zip
 unzip SDL-devel-1.2.15-VC.zip
 :SDL_copy
-copy SDL-1.2.15\include\ ..\gpac_public\extra_lib\include\SDL\
-copy SDL-1.2.15\lib\x86\*.lib ..\gpac_public\extra_lib\lib\win32\debug\
-copy SDL-1.2.15\lib\x86\*.lib ..\gpac_public\extra_lib\lib\win32\release\
-copy SDL-1.2.15\lib\x86\*.dll ..\gpac_public\bin\win32\debug\
-copy SDL-1.2.15\lib\x86\*.dll ..\gpac_public\bin\win32\release\
+copy SDL-1.2.15\include\ ..\Access_open\extra_lib\include\SDL\
+copy SDL-1.2.15\lib\x86\*.lib ..\Access_open\extra_lib\lib\win32\debug\
+copy SDL-1.2.15\lib\x86\*.lib ..\Access_open\extra_lib\lib\win32\release\
+copy SDL-1.2.15\lib\x86\*.dll ..\Access_open\bin\win32\debug\
+copy SDL-1.2.15\lib\x86\*.dll ..\Access_open\bin\win32\release\
 IF "%1"=="all" GOTO win64
 GOTO done
 
@@ -84,28 +84,28 @@ echo Copying x64 extralibs
 copy lib\x64\release\*eay* lib\x64\debug\
 copy lib\x64\release\av* lib\x64\debug\
 copy lib\x64\release\swscale* lib\x64\debug\
-copy lib\x64\debug\*.lib ..\gpac_public\extra_lib\lib\x64\debug\
-copy lib\x64\release\*.lib ..\gpac_public\extra_lib\lib\x64\release\
-copy lib\x64\debug\*.dll ..\gpac_public\bin\x64\debug\
-copy lib\x64\release\*.dll ..\gpac_public\bin\x64\release\
-copy lib\x64\release\*.manifest ..\gpac_public\bin\x64\release\
+copy lib\x64\debug\*.lib ..\Access_open\extra_lib\lib\x64\debug\
+copy lib\x64\release\*.lib ..\Access_open\extra_lib\lib\x64\release\
+copy lib\x64\debug\*.dll ..\Access_open\bin\x64\debug\
+copy lib\x64\release\*.dll ..\Access_open\bin\x64\release\
+copy lib\x64\release\*.manifest ..\Access_open\bin\x64\release\
 
 if exist xulrunner-7.0.1.en-US.win32.sdk.zip goto XULSDK_copy
 wget --no-check-certificate https://ftp.mozilla.org/pub/mozilla.org/mozilla.org/xulrunner/releases/7.0.1/sdk/xulrunner-7.0.1.en-US.win32.sdk.zip
 unzip -n xulrunner-7.0.1.en-US.win32.sdk.zip
 :XULSDK_copy
-xcopy /i /e /q /y xulrunner-sdk ..\gpac_public\extra_lib\include\xulrunner-sdk\
+xcopy /i /e /q /y xulrunner-sdk ..\Access_open\extra_lib\include\xulrunner-sdk\
 :DekTec_copy
 unzip -n dektec_dtapi_5.2.zip
 if exist SDL-devel-1.2.15-VC.zip goto SDL_copy
 wget http://www.libsdl.org/release/SDL-devel-1.2.15-VC.zip
 unzip SDL-devel-1.2.15-VC.zip
 :SDL_copy
-copy SDL-1.2.15\include\ ..\gpac_public\extra_lib\include\SDL\
-copy SDL-1.2.15\lib\x64\*.lib ..\gpac_public\extra_lib\lib\x64\debug\
-copy SDL-1.2.15\lib\x64\*.lib ..\gpac_public\extra_lib\lib\x64\release\
-copy SDL-1.2.15\lib\x64\*.dll ..\gpac_public\bin\x64\debug\
-copy SDL-1.2.15\lib\x64\*.dll ..\gpac_public\bin\x64\release\
+copy SDL-1.2.15\include\ ..\Access_open\extra_lib\include\SDL\
+copy SDL-1.2.15\lib\x64\*.lib ..\Access_open\extra_lib\lib\x64\debug\
+copy SDL-1.2.15\lib\x64\*.lib ..\Access_open\extra_lib\lib\x64\release\
+copy SDL-1.2.15\lib\x64\*.dll ..\Access_open\bin\x64\debug\
+copy SDL-1.2.15\lib\x64\*.dll ..\Access_open\bin\x64\release\
 GOTO done
 
 :MissingParameters
